f857477eae93d3201122401077b876c7b1cf0695
Arabinda Moni <arabindamoni@gmail.com>
2023-03-24 17:04:45 +0000

    Adding animation and mac backspace support.

--- script.js
+++ script.js
@@ -16,7 +16,19 @@ function calculate() {
 	}
 }
 
-document.addEventListener('keydown', function(event) {
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+	button.addEventListener('click', () => {
+		button.classList.add('clicked');
+		setTimeout(() => {
+			button.classList.remove('clicked');
+		}, 150);
+	});
+});
+
+
+document.addEventListener('keydown', function (event) {
 	if (event.key >= 0 && event.key <= 9) {
 		appendValue(event.key);
 	} else if (event.key === '.') {
@@ -27,7 +39,7 @@ document.addEventListener('keydown', function(event) {
 		calculate();
 	} else if (event.key === 'Escape') {
 		clearDisplay();
-	}else if (event.key === 'Backspace') {
-        display.textContent = display.textContent.slice(0, -1);
-    }
+	} else if (event.key === 'Backspace' || event.key === 'Delete' || event.key === 'U+0008') {
+		display.textContent = display.textContent.slice(0, -1);
+	}
 });
